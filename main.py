2d85db1684260b10a5846fcc0efe363af725ac34
Gabriel Freitas <gabriel.oliveira.1265286@sga.pucminas.br>
2024-11-15 17:34:22 -0300

    Correção carga dw chaves estrangeiras

--- main.py
+++ main.py
@@ -25,25 +25,19 @@ def execute_etl():
     try:
         conexao_original, engine_dw = get_database_connections()
         
-        # Queries para extrair dados
+        # Primeiro carregamos as dimensões
         queries = {
             'familia': "SELECT recebe_auxilio, cesta as recebeu_cesta FROM visitassertaodeminas",
             'comunidade': "SELECT DISTINCT comunidade, latitude, longitude FROM visitassertaodeminas",
             'data': "SELECT DISTINCT created_at FROM visitassertaodeminas",
-            'fato': """SELECT cesta_quantity, num_homens, num_mulheres, 
-                    sapatos as num_sapatos, kit_higiene as num_kit_higiene,
-                    leite as num_leite, brinquedos as num_brinquedos,
-                    biscoitos as num_biscoitos, racao as num_racao,
-                    roupas as num_roupas FROM visitassertaodeminas"""
         }
         
-        # Extrair dados
         dataframes = {
             name: pd.read_sql(query, conexao_original) 
             for name, query in queries.items()
         }
         
-        # Limpar tabelas existentes
+        # Limpar tabelas
         with engine_dw.connect() as conn:
             conn.execute(text("DELETE FROM fato_doacao"))
             conn.execute(text("DELETE FROM dim_familia"))
@@ -55,7 +49,34 @@ def execute_etl():
         dataframes['familia'].to_sql('dim_familia', engine_dw, if_exists='append', index=False)
         dataframes['comunidade'].to_sql('dim_comunidade', engine_dw, if_exists='append', index=False)
         dataframes['data'].to_sql('dim_datacriacao', engine_dw, if_exists='append', index=False)
-        dataframes['fato'].to_sql('fato_doacao', engine_dw, if_exists='append', index=False)
+        
+        # Agora vamos buscar os IDs gerados
+        with engine_dw.connect() as conn:
+            # Mapear IDs das dimensões
+            df_comunidades = pd.read_sql(text("SELECT idComunidade_PK, comunidade FROM dim_comunidade"), conn)
+            df_familia = pd.read_sql(text("SELECT idFamilia_PK FROM dim_familia ORDER BY idFamilia_PK"), conn)
+            df_data = pd.read_sql(text("SELECT idDataCriacao_PK FROM dim_datacriacao ORDER BY idDataCriacao_PK"), conn)
+            
+            # Carregar dados para a fato
+            query_fato = """SELECT 
+                cesta_quantity, num_homens, num_mulheres, 
+                sapatos as num_sapatos, kit_higiene as num_kit_higiene,
+                leite as num_leite, brinquedos as num_brinquedos,
+                biscoitos as num_biscoitos, racao as num_racao,
+                roupas as num_roupas, comunidade
+                FROM visitassertaodeminas"""
+            
+            df_fato = pd.read_sql(query_fato, conexao_original)
+            
+            # Mapear as chaves estrangeiras
+            comunidade_mapping = dict(zip(df_comunidades['comunidade'], df_comunidades['idComunidade_PK']))
+            df_fato['idComunidade_FK'] = df_fato['comunidade'].map(comunidade_mapping)
+            df_fato['idFamilia_FK'] = df_familia['idFamilia_PK'].values
+            df_fato['idDataCriacao_FK'] = df_data['idDataCriacao_PK'].values
+            
+            # Remover coluna auxiliar e carregar fato
+            df_fato = df_fato.drop('comunidade', axis=1)
+            df_fato.to_sql('fato_doacao', engine_dw, if_exists='append', index=False)
         
         return True
         
@@ -65,7 +86,7 @@ def execute_etl():
     finally:
         if 'conexao_original' in locals():
             conexao_original.close()
-
+            
 @app.route('/executar-etl', methods=['POST'])
 def handle_etl():
     try:
