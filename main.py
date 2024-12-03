cc6e5cc09245e1fc8ef4a6e3ceb7c238692bd202
Gabriel Freitas <gabriel.oliveira.1265286@sga.pucminas.br>
2024-12-03 19:23:28 -0300

    Atualizado port

--- main.py
+++ main.py
@@ -91,6 +91,9 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(debug=True, port=5500)
+
 @app.route("/")
 def home():
     return render_template("index.html")
